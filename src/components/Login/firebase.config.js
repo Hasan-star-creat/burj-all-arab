c19598df8104dc466ecb1ced167c8768962af159
Hasan Alli <hasanali3150927@gmail.com>
2021-06-09 21:55:03 +0600

    first commit

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,10 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAsihSADFnF_FoZ6je8p1W9lilyqX7cHWQ",
-    authDomain: "burj-al-arab.firebaseapp.com",
-    databaseURL: "https://burj-al-arab.firebaseio.com",
-    projectId: "burj-al-arab",
-    storageBucket: "burj-al-arab.appspot.com",
-    messagingSenderId: "244522202387",
-    appId: "1:244522202387:web:f3801d0588387140e1d6e6"
-  };
+  apiKey: "AIzaSyCw0pQxEK3zy0OGR4p9OPjuNccrZz9IQbA",
+  authDomain: "burj-all-arab-app.firebaseapp.com",
+  projectId: "burj-all-arab-app",
+  storageBucket: "burj-all-arab-app.appspot.com",
+  messagingSenderId: "87880508586",
+  appId: "1:87880508586:web:a56077afc5a66744fc1cd3"
+};
   export default firebaseConfig;
